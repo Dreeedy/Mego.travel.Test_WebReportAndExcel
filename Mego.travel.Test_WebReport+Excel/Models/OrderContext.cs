fac09b2de38e1708c79ea047858e67359b3cd5ca
Dreeedy <dreeedyone@gmail.com>
2021-11-22 15:14:02 +0300

    feat: Контекcт данных для заказов

---
+++ Mego.travel.Test_WebReport+Excel/Models/OrderContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mego.travel.Test_WebReport_Excel.Models
+{
+    /// <summary>
+    /// Контекcт данных для заказов
+    /// </summary>
+    public class OrderContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+            // По умолчанию у нас база данных отсутствуют.
+            // Поэтому в конструктор MobileContext определен вызов Database.EnsureCreated(),
+            // который при отсутствии базы данных автоматически создает ее.
+            // Если база данных уже есть, то ничего не происходит.
+            Database.EnsureCreated();
+        }
+    }
+}
