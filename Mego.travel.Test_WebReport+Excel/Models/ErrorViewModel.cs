3072358b701b03338691aeb09758d7b9592ada27
Dreeedy <dreeedyone@gmail.com>
2021-11-22 14:47:18 +0300

    Add project files.

---
+++ Mego.travel.Test_WebReport+Excel/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mego.travel.Test_WebReport_Excel.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
