5d07f979f3a890d2fa37011b8d235b1ff88968c4
Dreeedy <dreeedyone@gmail.com>
2021-11-22 17:56:12 +0300

    feat: заглушки для CRUD и базовый R

--- Mego.travel.Test_WebReport+Excel/Controllers/HomeController.cs
+++ Mego.travel.Test_WebReport+Excel/Controllers/HomeController.cs
@@ -12,15 +12,75 @@ namespace Mego.travel.Test_WebReport_Excel.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private OrderContext _orderContext;
 
-        public HomeController(ILogger<HomeController> logger)
+/*        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+        }*/
+
+        public HomeController(OrderContext orderContext)
+        {
+            _orderContext = orderContext;
+        }
+
+        [HttpGet]
+        public IActionResult Create(int? id)
+        {
+            if (id == null) return RedirectToAction("Index");
+            ViewBag.OrderId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Create(Order order)
+        {
+            _orderContext.Orders.Add(order);
+
+
+            // сохраняем в бд все изменения
+            _orderContext.SaveChanges();
+            return "200";
         }
 
         public IActionResult Index()
         {
+            return View(_orderContext.Orders.ToList());// Передача данных на view
+        }
+
+        [HttpGet]
+        public IActionResult Update(int? id)
+        {
+            if (id == null) return RedirectToAction("Index");
+            ViewBag.OrderId = id;
             return View();
+        }
+        [HttpPost]
+        public string Update(Order order)
+        {
+            _orderContext.Orders.Add(order);
+
+
+            // сохраняем в бд все изменения
+            _orderContext.SaveChanges();
+            return "200";
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null) return RedirectToAction("Index");
+            ViewBag.OrderId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Delete(Order order)
+        {
+            _orderContext.Orders.Add(order);
+
+
+            // сохраняем в бд все изменения
+            _orderContext.SaveChanges();
+            return "200";
         }
 
         public IActionResult Privacy()
