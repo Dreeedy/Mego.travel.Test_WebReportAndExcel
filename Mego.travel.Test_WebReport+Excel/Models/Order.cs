1640c51c2a305d70d02de483a5326820b5a254d4
Dreeedy <dreeedyone@gmail.com>
2021-11-22 14:59:05 +0300

    feat: модель для работы с заказами

---
+++ Mego.travel.Test_WebReport+Excel/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mego.travel.Test_WebReport_Excel.Models
+{
+    /// <summary>
+    /// Модель для заказов
+    /// </summary>
+    public class Order
+    {
+        public int Id { get; set; }        
+        public int Price { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
